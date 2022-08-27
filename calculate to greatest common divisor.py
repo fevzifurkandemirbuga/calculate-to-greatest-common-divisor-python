f4703efc9b2af0432b199b08495b163a497b882c
FFD <2010213003@ogrenci.karabuk.edu.tr>
2022-08-28 01:49:13 +0300

    calculate to greatest common divisor in pyton
    calculate to greatest common divisor in pyton

---
+++ calculate to greatest common divisor.py
@@ -0,0 +1,13 @@
+def gcd(x, y):
+    i = 1
+    result = 1
+    while i <= x and i <= y:
+        if not (x % i) and not (y % i):
+            result = i
+        i += 1
+    return result
+
+
+a = int(input("enter two number\n"))
+b = int(input())
+print("greatest common divisor:", gcd(a, b))
