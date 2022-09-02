1569944551527cb060a05bc2e90757d0bf0e2118
Fevzi Furkan Demirbuğa <103340757+fevzifurkandemirbuga@users.noreply.github.com>
2022-09-02 09:07:52 +0300

    Update calculate to greatest common divisor.py

--- calculate to greatest common divisor.py
+++ calculate to greatest common divisor.py
@@ -1,5 +1,5 @@
 def gcd(x, y):
-    i = 1
+    i = 1 
     result = 1
     while i <= x and i <= y:
         if not (x % i) and not (y % i):
@@ -8,6 +8,6 @@ def gcd(x, y):
     return result
 
 
-a = int(input("enter two number\n"))
-b = int(input())
+a = int(input("enter first number: "))
+b = int(input("enter second number: ))
 print("greatest common divisor:", gcd(a, b))
